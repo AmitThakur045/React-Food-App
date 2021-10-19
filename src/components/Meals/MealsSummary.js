8cfb416101b4156716ab09a70a469c4fb7f6c80f
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-10-19 20:50:02 +0530

    Initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered to you</h2>
+      <p>
+        Choose Your favorite meal from out brand election of available meals and
+        enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just in time and
+        ofcourse by experienced chefs!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
