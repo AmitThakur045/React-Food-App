e938863f616a6533b4144565d2d42d09fc36e905
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-12-23 20:38:40 +0530

    initial commit

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -73,11 +77,16 @@ const CardProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
-    removeItem: removeItemFromHandler
+    removeItem: removeItemFromHandler,
+    clearCart: clearCartHandler
   };
 
   return (
